0116dc02cef77a351a2006d045d63c68c0f5cd72
jackville <jackson.eeeppr@gmail.com>
2025-08-16 17:48:08 -0300

    Update CartasSuperTrunfo.c
    Ajustes para melhor entendimento do código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,35 +2,29 @@
 
 int main (){
 
-    char estado1;
-    char estado2;
-    char codigodacarta1[4];
-    char codigodacarta2[4];
-    char nomedacidade1[15];
-    char nomedacidade2[15];
-    int populacao1;
-    int populacao2;
-    float area1;
-    float area2;
-    float pib1;
-    float pib2;
-    int pontosturisticos1;
-    int pontosturisticos2;
+    // VARIÁVEIS DE ENTRADA DE CARTAS 1 E 2
+    char estado1, estado2;
+    char codigodacarta1[4], codigodacarta2[4];
+    char nomedacidade1[25], nomedacidade2[25];
+    int populacao1, populacao2;
+    int pontosturisticos1, pontosturisticos2;
+    float area1, area2;
+    float pib1, pib2;
 
-    printf("Bem vindo ao sistema de cartas!\n");
+    printf("Bem vindo ao sistema de CARTAS SUPER TRUNFO!\n");
     printf("Você irá cadastrar informações de duas cartas diferentes!\n\n");
 
-    // CARTA 01 - INFORMAÇÕES
-    printf("Carta 1\n");
+    // CARTA 1 - ENTRADA DE INFORMAÇÕES
+    printf("--------------- CARTA 1 ---------------\n");
 
     printf("Digite uma letra de 'A' a 'H' (representando um estado)\n");
     scanf(" %c", &estado1);
 
     printf("Repita a letra do estado, e acrescente um número de '01' a '04'\n");
-    scanf("%4s", codigodacarta1);
+    scanf("%3s", codigodacarta1);
 
-    printf("Digite o nome de uma cidade!");
-    scanf("%s", nomedacidade1);
+    printf("Digite o nome de uma cidade!\n");
+    scanf("%24s", nomedacidade1);
 
     printf("Digite o número de habitantes da cidade!\n");
     scanf("%d", &populacao1);
@@ -44,20 +38,19 @@ int main (){
     printf("Digite em números, a quantidade de pontos turísticos da cidade!\n");
     scanf("%d", &pontosturisticos1);
 
-    printf("Informações Cadastradas com sucesso!\n");
+    printf("Informações Cadastradas com sucesso!\n\n");
 
-
-    // CARTA 02 - INFORMAÇÕES
-    printf("\nInsira as informações da carta 2!\n");
+    // CARTA 2 - ENTRADA DE INFORMAÇÕES
+    printf("--------------- CARTA 2 ---------------\n");
 
     printf("Digite uma letra de 'A' a 'H' (representando um estado)\n");
     scanf(" %c", &estado2);
 
     printf("Repita a letra do estado, e acrescente um número de '01' a '04'\n");
-    scanf("%4s", codigodacarta2);
+    scanf("%3s", codigodacarta2);
 
     printf("Digite o nome de uma cidade!\n");
-    scanf("%s", nomedacidade2);
+    scanf("%24s", nomedacidade2);
 
     printf("Digite o número de habitantes da cidade!\n");
     scanf("%d", &populacao2);
@@ -72,7 +65,7 @@ int main (){
     scanf("%d", &pontosturisticos2);
 
     // SAÍDA DE DADOS CADASTRADOS!
-    printf("\nCarta 1:\n");
+    printf("\n-------- CARTA 1 --------\n");
     printf("Estado: %c\n", estado1);
     printf("Código: %s\n", codigodacarta1);
     printf("Nome da Cidade: %s\n", nomedacidade1);
@@ -81,7 +74,7 @@ int main (){
     printf("PIB: %.2f\n", pib1);
     printf("Número de Pontos Turísticos: %d\n", pontosturisticos1);
 
-    printf("\nCarta 2:\n");
+    printf("\n-------- CARTA 2 --------\n");
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigodacarta2);
     printf("Nome da Cidade: %s\n", nomedacidade2);
